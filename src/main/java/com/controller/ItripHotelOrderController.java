3877cb4ac91c6f359459518c3e1e6bdb591450e6
我就是我 <3035195148@qq.com>
2020-12-18 17:52:18 +0800

    biz

--- src/main/java/com/controller/ItripHotelOrderController.java
+++ src/main/java/com/controller/ItripHotelOrderController.java
@@ -1,50 +1,47 @@
 package com.controller;
 
-
 import com.po.Dto;
 import com.serivce.ItripHotelTempStoreService;
+import com.util.DtoUtil;
 import com.util.vo.RoomStoreVO;
+import com.util.vo.StoreVO;
 import com.util.vo.ValidateRoomStoreVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/hotelorder/")
 public class ItripHotelOrderController {
-@Autowired
-private ItripHotelTempStoreService itripHotelTempStoreService;
-@RequestMapping(value = "/getpreorderinfo")
-    public Dto<RoomStoreVO> getpreorderinfo(HttpServletRequest request, HttpServletResponse response, @PathVariable ValidateRoomStoreVO validateRoomStoreVO){
-    System.out.println("生成订单前,获取预订信息方法进入。。。");
-    String tocken=request.getHeader("token");
-    System.out.println("token==="+tocken);
-    Map<String,Object> map=new HashMap<>();
-    return null;
-}
-
-
-
-/*@RequestMapping(value = "/getpersonalorderlist")
-    public Dto getpersonalorderlist(@RequestBody ItripSearchOrderVO itripSearchOrderVO){
-    System.out.println("查询个人订单列表，分页显示方法进入。。。。");
-    return null;
-}
-@RequestMapping(value = "/getpersonalorderinfo/{orderId}")
-    public Dto getpersonalorderinfo(@PathVariable Long orderId){
-    System.out.println("根据订单ID查看个人订单详情方法进入。。。。。");
-    List<TtripHotelOrder> orderList=itripHotelOrderSerivce.finddingdanAll(orderId);
-    return DtoUtil.returnDataSuccess(orderList);
-}
-@RequestMapping(value = "/getpersonalorderroominfo/{orderId}")
-    public Dto getpersonalorderroominfo(@PathVariable Long orderId){
-    System.out.println("根据订单ID查看个人订单详情-房型相关信息方法进入。。。。。");
-    return null;
-}*/
+    @Autowired
+    private ItripHotelTempStoreService itripHotelTempStoreService;
+        /**通过酒店id和房间Id加预定时间查询相关信息(订单生成前信息展示)**/
+    @RequestMapping(value = "/getpreorderinfo")
+    public Dto<RoomStoreVO> getpreorderinfo(HttpServletRequest request, HttpServletResponse response, @RequestBody ValidateRoomStoreVO validateRoomStoreVO){
+        System.out.println("通过酒店id和房间Id加预定时间查询相关信息(订单生成前信息展示)方法.........");
+        Map<String,Object> param=new HashMap<>();
+        try {
+        //1.判断用户是否存在(从浏览器获取token，redis获取用户信息)
+           String token=request.getHeader("token");
+            System.out.println("token:"+token);
+        //2.通过酒店id获取酒店名称
+        //3.剩余房间数量
+        param.put("roomId",validateRoomStoreVO.getRoomId());
+        param.put("startTime",validateRoomStoreVO.getCheckInDate());
+        param.put("endTime",validateRoomStoreVO.getCheckOutDate());
+        List<StoreVO> storeVOList=itripHotelTempStoreService.queryRoomStore(param);
+        //4.房费运算
+        //5.综合RoomStoreVO
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
